acc1d154a835ea7d2ccf04b0cfc2e8c27a2449ac
DanStarflower <cpedreros@utem.cl>
2024-10-29 14:16:48 -0300

    325235235

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import qrcode
+import matplotlib.pyplot as plt
+import os
+import time
 
 # Cargar el archivo CSV
 df_productos = pd.read_csv('InventarioPruebas.csv', encoding="utf-8", sep=";")
@@ -68,6 +72,8 @@ def eliminar_producto():
     print(f"Producto del tipo '{tipo}', colegio '{colegio}' y talla '{talla}' eliminado.")
 
     print(df_productos.head())
+
+
 def Agregar_producto():
     # Solicitar los datos del nuevo producto al usuario
     nuevo_producto = {
@@ -79,10 +85,28 @@ def Agregar_producto():
 
     # Convertir los datos en un DataFrame y añadir a los datos existentes
     nuevo_producto_df = pd.DataFrame([nuevo_producto])
-    
-    # Añadir el nuevo producto al DataFrame existente 'df_productos'
     global df_productos
     df_productos = pd.concat([df_productos, nuevo_producto_df], ignore_index=True)
+
+    # Generar el código QR con los datos del producto (sin la cantidad)
+    data_qr = f"{nuevo_producto['Tipo_producto']}\n{nuevo_producto['Colegio_producto']}\n{nuevo_producto['Talla_producto']}"
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data_qr)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    ruta_carpeta_qr = "QRs productos"
+    os.makedirs(ruta_carpeta_qr, exist_ok=True)
+    id_unico = int(time.time())
+
+    # Guardar el código QR como una imagen
+    img.save(os.path.join(ruta_carpeta_qr, f"qr_producto_{id_unico}.png"))
+    print("Código QR generado y guardado")
 
     # Guardar los datos actualizados en el archivo CSV
     df_productos.to_csv('InventarioPruebas.csv', sep=';', index=False)
